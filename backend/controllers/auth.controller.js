24bca6cb99e56656f724ad75552eccda3fdd642c
DebugDynamo <er.ritikpal@gmail.com>
2025-01-26 18:27:29 +0530

    added middleware and fn to handle error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({
@@ -16,6 +16,6 @@ export const signup = async (req, res) => {
       message: "User Created Successfully",
     });
   } catch (error) {
-    res.status(500).json(error.message);
+   next(error);
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -28,3 +28,14 @@ console.log(`server is running on port ${PORT}`)
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+// middleware
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
