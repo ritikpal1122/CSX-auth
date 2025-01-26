406f817edf673cbf47f46283437c7e47414550bf
DebugDynamo <er.ritikpal@gmail.com>
2025-01-26 13:33:01 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express'
+import mongoose from 'mongoose';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("connected to database")
+})
+.catch((err)=>{
+    console.log(err)
+});
 
 const app = express();
 const PORT = 3000;
